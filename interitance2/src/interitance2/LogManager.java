d82986caa51571f6c532405d376e6aadf73210cf
Talha Arıç <talharic@gmail.com>
2021-10-17 23:08:43 +0300

    inheritance2

---
+++ interitance2/src/interitance2/LogManager.java
@@ -0,0 +1,11 @@
+package interitance2;
+
+public class LogManager {
+	public void log(int logType) {
+		
+	}
+}
+
+//1 - Database
+//2 - File
+//3 - Email
---
+++ interitance2/src/interitance2/Main.java
@@ -0,0 +1,9 @@
+package interitance2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.add(new DatabaseLogger());
+	}
+}
